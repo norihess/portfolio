a21c00a131fbeb28b9c0d820e77ecb8e075bbda4
norihess <Norihess19@gmail.com>
2022-06-23 11:30:42 -0600

    created an updated files

---
+++ components /ServiceCard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ServiceCard = ({ name, description, tech }) => {
+  return (
+    <div className="w-full p-4 mob:p-2 rounded-lg transition-all ease-out duration-300 hover:bg-slate-50 hover:scale-105 cursor-pointer">
+      <h1 className="text-3xl">{name ? name : "Heading"}</h1>
+      <p className="mt-5 opacity-40 text-xl">
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default ServiceCard;
---
+++ yourData .js
@@ -0,0 +1,141 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Nori Hess",
+  headerTaglineOne: "Hello 👋",
+  headerTaglineTwo:
+    "My name is Nori and i'm a web developer with a background of design.",
+
+    socials: [
+      { title: "Github", link: "https://github.com/norihess" },
+      { title: "Twitter", link: "https://twitter.com/HessNori" },
+      { title: "LinkedIn", link: "https://www.linkedin.com/in/nori-hess-977771200/" },
+      { title: "Meduim", link: "https://medium.com/@norihess19" },
+      { title: "Email", link: "mailto:norihess19@gmail.com" },
+    ],
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Angular-Flix", //Project Title - Add Your Project Title Here
+      description: `Using Angular, build the client-side for an application called myFlix based on
+      its existing server-side code (REST API and database), with supporting
+      documentation. Technology used: React | Bootstrap| Heroku | Netlify | MongoDB`,
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "../assets/img/angular.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://the-great-norihess-site.netlify.app/welcome",
+    },
+    {
+      title: "Meet App", //Project Title - Add Your Project Title Here
+      description: `This API allows a users to access a database to receive information on movies such as the movies' directors and genre. 
+      The user can sign up, update personal information and save/delete favorite movies to/from his personal profile.
+      Technology used: React | MUI | Jest & Jest Cucumber | Puppeteer | GoogleApis | AWS`,
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "../assets/img/meet2.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://norihess.github.io/meetup/",
+    },
+    {
+      title: "React Movie Flix", //Project Title - Add Your Project Title Here
+      description: `This is a single page app that uses state routing and a responsive UI library for a seamless, smooth experience. 
+      The app provides movies from a dedicated API and allows users to view information about the movies, Directors and Genres, and add/remove Movies to a list of favorites.
+      New users are able register, registered users can login, interact with the app and logout. 
+      Technologies used: React | Bootstrap | Node.js | Express`,
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "../assets/img/react_mf.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/norihess/myFlix-client",
+    },
+    {
+      title: "Server-side Movie Api", //Project Title - Add Your Project Title Here
+      description: `The web application is based on its existing server-side code MyFlix (REST API and database). 
+      Technology used were React | Bootstrap | Node.js | Express`,
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "../assets/img/ss_mf.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/norihess/movie_api",
+    },
+    {
+      title: "Pokedex", //Project Title - Add Your Project Title Here
+      description: `The aim of this project is to build a Pokedex app that will retrieve information from an API to display on a usable app.
+      This is a small application that loads Pokémon from an external API and allows to display their details.
+      Technology used: Bootstrap | JavaScript | JQuery`,
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "../assets/img/poke2.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/norihess/simple_js_app",
+    },
+    {
+      title: "To Do List App", //Project Title - Add Your Project Title Here
+      description: `This is a small web application that allows users to add items to a to do list, cross out items, delete items and sort the items on the list.",
+      Technology used: Bootstrap | JavaScript | JQuery`,
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "../assets/img/todo.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/norihess/to-do-list-app",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        description: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // Services Section ---------------
+
+  services: [
+    {
+      title: "Art Direction",
+      description:
+        "We help with the creation and development of online advertising ideas, with particular focus on their visual appearance.",
+    },
+    {
+      title: "Branding",
+      description:
+        "I can design key brand elements such as the logo, color scheme, typography, and other design components that makes your brand stand out from competitors.",
+    },
+    {
+      title: "Web Design",
+      description:
+        "I can build and optimize your online presence.  Website is the digital entry point into your business and a powerful revenue channel.",
+    },
+    // {
+    //   title: "3D Design",
+    //   description:
+    //     "We combine creative design and technical skills to build striking 3D visualisations that bring your project to life.",
+    // },
+  ],
+
+  // End Services Section --------------------------
+
+  // About Secton --------------
+  aboutpara:
+    `
+    Hello, my name is Nori Hess and i'm a web developer with a background of design. My past roles taught me how to identify and find creative solutions while working with multidisciplinary methodologies.
+
+I'm traditioning into development becasue I have a strong affinity for problem solving and developing apps, websites, ect beyond looks. I love a good challange and full-stack development is going to keep my on my toes and my mind sharp. My goal is to become a full-stack developer, who happens to be able to design as well. I believe being able to know the methodologies behind UI/UX and development will help me be successful in the future.
+
+I'm currently living in the Denver area and I'm ready to tackle the workforce!
+    `,
+  //   End About Section ---------------------
+};
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
